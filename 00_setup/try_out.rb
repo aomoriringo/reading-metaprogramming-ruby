32bef1122cf3fad2a8753d1322eb50e9084329fc
aomoriringo <sel0433@gmail.com>
2019-11-28 15:00:22 +0900

    matofy

--- 00_setup/try_out.rb
+++ 00_setup/try_out.rb
@@ -5,21 +5,16 @@ class TryOut
   # first_name=メソッドを持つ。これは、引数の内容でファーストネームを書き換える。
   # upcase_full_nameメソッドを持つ。これは、full_nameメソッドの結果をすべて大文字で返す。このメソッドは副作用を持たない。
   # upcase_full_name! メソッドを持つ。これは、upcase_full_nameの副作用を持つバージョンで、ファーストネーム、ミドルネーム、ラストネームをすべて大文字に変え、オブジェクトはその状態を記憶する
-  def initialize(*args)
-    if args.length == 2
-      @first, @last = args
-      @middle = ''
-    else
-      @first, @middle, @last = args
-    end
+  attr_accessor :first_name
+
+  def initialize(first_name, middle_name = "", last_name)
+    @first_name = first_name
+    @middle_name = middle_name
+    @last_name = last_name
   end
 
   def full_name
-    if @middle.empty?
-      [@first, @last].join(' ')
-    else
-      [@first, @middle, @last].join(' ')
-    end
+    [@first_name, @middle_name, @last_name].reject(&:empty?).join(' ')
   end
 
   def upcase_full_name
@@ -27,14 +22,10 @@ class TryOut
   end
 
   def upcase_full_name!
-    @first.upcase!
-    @middle.upcase!
-    @last.upcase!
-    full_name.upcase
-  end
-
-  def first_name=(v)
-    @first = v
+    @first_name.upcase!
+    @middle_name.upcase!
+    @last_name.upcase!
+    full_name
   end
 
 end
