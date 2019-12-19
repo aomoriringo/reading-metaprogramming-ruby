343903ff2572ee5d65c158a188535585208a66d9
aomoriringo <sel0433@gmail.com>
2019-12-19 15:11:40 +0900

    Solve 3 - 3.2.4

--- 03_method/define.rb
+++ 03_method/define.rb
@@ -14,3 +14,48 @@
 # 次の動作をする OriginalAccessor モジュール を実装する
 # - OriginalAccessorモジュールはincludeされたときのみ、my_attr_accessorメソッドを定義すること
 # - my_attr_accessorはgetter/setterに加えて、boolean値を代入した際のみ真偽値判定を行うaccessorと同名の?メソッドができること
+
+
+class A1
+  define_method "//" do
+    "//"
+  end
+end
+
+class A2
+  def initialize(arr)
+    arr.each do |s|
+      A2.define_component s
+    end
+  end
+
+  def self.define_component(name)
+    define_method("hoge_#{name}") do |i|
+      if i.nil?
+        dev_team
+      else
+        __method__.to_s*i
+      end
+    end
+  end
+
+  def dev_team
+    "SmartHR Dev Team"
+  end
+end
+
+module OriginalAccessor
+  def self.included(c)
+    def c.my_attr_accessor(v)
+      attr_reader v
+      define_method "#{v}=" do |val|
+        instance_variable_set("@#{v}", val)
+        if (!!val === val)
+          define_singleton_method "#{v}?" do
+            return val
+          end
+        end
+      end
+    end
+  end
+end
